b4d3d601a5b40816c419d6c3c66794241f472284
Michael-IRL <iszekeell@gmial.com>
2022-05-04 11:35:14 +0100

    inital commit

---
+++ webpack.dev.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // This option controls if and how source maps are generated.
+  // https://webpack.js.org/configuration/devtool/
+  devtool: 'source-map',
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './src/js/index.js',
+    about: './src/js/about.js',
+    contacts: './src/js/contacts.js',
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8080,
+  },
+
+  // https://webpack.js.org/concepts/loaders/
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/about.html',
+      inject: true,
+      chunks: ['about'],
+      filename: 'about.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/contact.html',
+      inject: true,
+      chunks: ['contacts'],
+      filename: 'contacts.html',
+    }),
+  ],
+};
